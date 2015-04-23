f303a85f1ee1883c3dc90af6cb7cb06ced406b93
Jayson Martinez <jlmartinez631@gmail.com>
2015-04-23 16:46:32 +0800

    Changed file Tbl_party to simply Party

--- application/models/registrar/Party.php
+++ application/models/registrar/Party.php
@@ -2,10 +2,16 @@
 	/**
 	* 
 	*/
-	class Tbl_party extends CI_Model
+	class Party extends CI_Model
 	{
 		function insert_students($data)
 		{
 			echo $data;
 		}
+
+        function searchId($id)
+        {
+            $party_id = $this->db->query("SELECT * FROM tbl_party WHERE legacyid LIKE '$id%' LIMIT 5");
+            return $party_id->result_array();
+        }
 	}
