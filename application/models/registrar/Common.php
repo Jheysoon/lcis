fe917c547a2798924bfd6122c3d6c00d11f03b8b
gregoriosonit <gregoriosonit@gmail.com>
2015-04-27 08:40:42 +0800

    Edited Common.php

--- application/models/registrar/Common.php
+++ application/models/registrar/Common.php
@@ -5,15 +5,14 @@
 	class Common extends CI_Model
 	{
 		function select_student($id){
-
 			$result = $this->db->query("SELECT tbl_party.id as partyid, legacyid, firstname, 
 				middlename, tbl_registration.coursemajor, tbl_registration.student, lastname,
 				`date`, course, tbl_course.id,description  FROM tbl_coursemajor, tbl_course,tbl_party,
-				 tbl_registration WHERE legacyid = '$id' 
+				tbl_registration WHERE legacyid = '$id' 
 				AND tbl_registration.student = tbl_party.id 
 				AND tbl_coursemajor.id = tbl_registration.coursemajor
 				AND tbl_course.id = tbl_coursemajor.course ORDER by `date` DESC");
-			return $result->row_array();
+				return $result->row_array();
 		}
 		function get_school($partyid){
 			$result = $this->db->query("SELECT school, firstname as sch
@@ -23,24 +22,25 @@
 		}
 		function select_schoolyear($partyid, $school){
 			$result = $this->db->query("SELECT academicterm FROM tbl_enrolment
-			 where student = '$partyid' AND  school = '$school' GROUP BY academicterm ORDER by academicterm");
-			return $result->result_array();
+				WHERE student = '$partyid' AND  school = '$school' GROUP BY academicterm ORDER by academicterm");
+				return $result->result_array();
 		}
 		function select_academicterm($academicterm){
 			$result = $this->db->query("SELECT systart, syend, tbl_academicterm.term,description 
 				FROM tbl_academicterm, tbl_term WHERE tbl_academicterm.id = '$academicterm' 
 				AND tbl_term.id = tbl_academicterm.term");
-			return $result->row_array();
+				return $result->row_array();
 		}
 		function select_enrolmentid($academicterm, $partyid){
 			$result = $this->db->query("SELECT id as enrolmentid FROM tbl_enrolment WHERE student = '$partyid' 
 				AND academicterm = '$academicterm'");
-			return $result->result_array();
+				return $result->result_array();
 		}
-		function get_all_grades($enrolmentid)	{
-			$result = $this->db->query("SELECT classallocation, semgrade, enrolment, subject, code, descriptivetitle, `value` FROM `tbl_studentgrade`, tbl_classallocation, tbl_subject, tbl_grade
- 			WHERE enrolment = '$enrolmentid' AND tbl_classallocation.id = classallocation AND tbl_subject.id = subject AND tbl_grade.id = semgrade");
-			return $result->result_array();
+		function get_all_grades($enrolmentid){
+			$result = $this->db->query("SELECT classallocation, semgrade, enrolment, subject, code, descriptivetitle, `value` 
+				FROM `tbl_studentgrade`, tbl_classallocation, tbl_subject, tbl_grade WHERE enrolment = '$enrolmentid' 
+				AND tbl_classallocation.id = classallocation AND tbl_subject.id = subject AND tbl_grade.id = semgrade");
+				return $result->result_array();
 		}
 
 
