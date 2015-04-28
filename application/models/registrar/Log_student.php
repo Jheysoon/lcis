f1208286a0f48a2936acdd10007a1b3c0426bf21
gregoriosonit <gregoriosonit@gmail.com>
2015-04-28 15:15:51 +0800

    Edited Views in Registrar and Clerk

--- application/models/registrar/Log_student.php
+++ application/models/registrar/Log_student.php
@@ -9,17 +9,14 @@ class Log_student extends CI_Model
     function insert_not_exists($partyid)
     {
         $date = date('Y-m-d');
-        $uid = $this->session->get_userdata('uid');
-        $status = $this->session->get_userdata('status');
-        $data = array('student' => $partyid,
-                        'dte' =>  $date,
-                        'user' => $uid,
-                        'status' => $status);
+        $uid = $this->session->userdata('uid');
+        $status = $this->session->userdata('status');
+        $data = array('student' => $partyid,'dte' =>  $date, 'user' => $uid, 'status' => $status);
         $this->db->insert('log_student', $data);
 
         $data2 = array('status' => $status);
         $this->db->where('id', $partyid);
-        $this->db->update('tbl_part', $data2);
+        $this->db->update('tbl_party', $data2);
 
     }
 }
