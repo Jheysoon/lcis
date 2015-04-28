85d0e2264ef0f5923ab865bab42be21b265350f6
vladz25 <lacabeoliver@gmail.com>
2015-04-28 10:36:09 +0800

    edited css and flag filter

--- application/views/registrar/student_list.php
+++ application/views/registrar/student_list.php
@@ -57,12 +57,13 @@
             <div class="col-md-6"><br/>
             <form class="navbar-form navbar-right" action="/registrar/search" method="post" role="search">
                 <label>Status Filter</label>
-                <select class="form-control" name="status">
-                    <option>Open</option>
-                    <option>Editing</option>
-                    <option>Submitted</option>
-                    <option>Confirmed</option>
-                    <option>Returned</option>
+                <select class="form-control" name="status" onchange="changeSession(this)">
+                    <option value="notopen">Not Open</option>
+                    <option value="open">Open</option>
+                    <option value="editing">Editing</option>
+                    <option value="submitted">Submitted</option>
+                    <option value="confirmed">Confirmed</option>
+                    <option value="returned">Returned</option>
                 </select>
                 <div class="form-group">
                     <input type="hidden" name="cur_url" value="<?php echo current_url(); ?>"/>
