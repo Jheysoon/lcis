43c22ae4e350ee8ead6b1c70c8638495ca49afa7
gregoriosonit <gregoriosonit@gmail.com>
2015-04-28 14:56:40 +0800

    INSERT LOGS IN STUDENT_LOG

--- application/models/registrar/Log_student.php
+++ application/models/registrar/Log_student.php
@@ -6,24 +6,16 @@
 
 class Log_student extends CI_Model
 {
-    function insert_not_exists($id)
+    function insert_not_exists($partyid)
     {
-        $this->db->trans_begin();
-        $this->db->where('student',$id);
-        $q = $this->db->count_all_results('log_student');
-        if($q < 1)
-        {
-            $data['student'] = $id;
-            $data['status'] = 'O';
-            $this->db->insert('log_student',$data);
-        }
-        if($this->db->trans_status() === FALSE)
-        {
-            $this->db->trans_rollback();
-        }
-        else
-        {
-            $this->db->trans_commit();
-        }
+        $date = date('Y-m-d');
+        $uid = $this->session->get_userdata('uid');
+        $status = $this->session->get_userdata('status');
+        $data = array('student' => $partyid,
+                        'dte' =>  $date,
+                        'user' => $uid,
+                        'status' => $status);
+        $this->db->insert('log_student', $data);
+
     }
 }
