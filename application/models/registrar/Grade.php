fc5e5bbbe84cfd65c63e5a7f034b6062ce2a1ae0
Jayson Martinez <jlmartinez631@gmail.com>
2015-05-07 09:24:53 +0800

    Added re-exam grade when grade is inc in ajax

--- application/models/registrar/Grade.php
+++ application/models/registrar/Grade.php
@@ -12,9 +12,17 @@ class Grade extends CI_Model{
     function getAllGrade()
     {
         //$this->db->cache_on();
+        $this->db->order_by('description ASC, value ASC');
         $q = $this->db->get('tbl_grade');
         //$this->db->cache_off();
         return $q->result_array();
+    }
+
+    function get_where($id)
+    {
+    	$this->db->where('id',$id);
+    	$q = $this->db->get('tbl_grade');
+    	return $q->row_array();
     }
 
 }
--- application/views/registrar/ajax/add_subject.php
+++ application/views/registrar/ajax/add_subject.php
@@ -5,26 +5,88 @@
 <td><?php echo $sub['code']; ?></td>
 <td><?php echo $sub['descriptivetitle']; ?></td>
 <td>
-    <select class="form-control" name="edit_sub_grade">
+    <?php 
+        $val1 = $this->grade->get_where($grade_user);
+        $val = $val1['value'];
+        $style = '';
+        if($val == 0.00)
+        {
+            $style = 'disabled';
+        }
+     ?>
+    <select class="form-control" name="edit_sub_grade" <?php echo $style; ?>>
         <?php
             $grade = $this->grade->getAllGrade();
             foreach($grade as $g)
             {
                 if($grade_user == $g['id']){
                     ?>
-                    <option value="<?php echo 'stugrade-'.$sid.'_subj-'.$g['id'].'_enroll-'.$enrolmentid; ?>" selected><?php echo $g['value']; ?></option>
+                    <option value="<?php echo 'stugrade-'.$sid.'_subj-'.$g['id'].'_enroll-'.$enrolmentid; ?>" selected>
+                    <?php
+                        if($g['value'] == 0.00)
+                            echo $g['description'];
+                        else
+                            echo $g['value']; 
+                    ?>
+                    </option>
                     <?php
                 }
                 else{
                     ?>
-                    <option value="<?php echo 'stugrade-'.$sid.'_subj-'.$g['id'].'_enroll-'.$enrolmentid; ?>"><?php echo $g['value']; ?></option>
+                    <option value="<?php echo 'stugrade-'.$sid.'_subj-'.$g['id'].'_enroll-'.$enrolmentid; ?>">
+                    <?php
+                        if($g['value'] == 0.00)
+                            echo $g['description'];
+                        else
+                            echo $g['value']; 
+                    ?>
+                    </option>
                     <?php
                 }
             }
         ?>
     </select>
 </td>
-<td> &nbsp; </td>
+<td>
+<?php 
+    $value1 = $this->grade->get_where($grade_user);
+    $value = $value1['value'];
+    if($value == 0.00) 
+    {
+        $reexam_grade = $this->studentgrade->get_reexam($sid);
+?>
+<select class="form-control rexam" name="re-exam">
+    <option value="1" selected>
+        Select
+    </option>
+    <?php 
+        $all_grade = $this->grade->getAllGrade();
+
+        foreach($all_grade as $ag)
+        {
+            if($ag['value'] != 0.00)
+            {
+                if($ag['id'] == $reexam_grade)
+                {
+        ?>
+                    <option value="<?php echo 'stugrade-'.$sid.'_subj-'.$ag['id'].'_enroll-'.$enrolmentid; ?>" selected>
+                        <?php echo $ag['value']; ?>
+                    </option>
+        <?php
+                }
+                else{
+                    ?>
+                    <option value="<?php echo 'stugrade-'.$sid.'_subj-'.$ag['id'].'_enroll-'.$enrolmentid; ?>">
+                        <?php echo $ag['value']; ?>
+                    </option>
+            <?php
+                }
+            }
+        }
+?>
+</select>
+<?php } ?>
+</td>
 <td class="text-right"><?php echo $sub['units']; ?></td>
     <td><a href="<?php echo $enrolmentid.'-'.$sid; ?>" class="btn btn-link del_sub">Delete</a></td>
 </tr>
