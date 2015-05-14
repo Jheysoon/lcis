f3173e29a8280275894f41622989f481746ee19d
gregoriosonit <gregoriosonit@gmail.com>
2015-05-14 16:00:27 +0800

    Filter Curriculum by UID

--- application/models/registrar/Curriculum.php
+++ application/models/registrar/Curriculum.php
@@ -34,15 +34,31 @@ class Curriculum extends CI_Model
 		return $result->result_array();
 	}
 	function getAllcurr(){
+		$uid = $this->session->userdata('uid');
+		$getCollege = $this->db->query("SELECT tbl_college.id as id, tbl_college.description as descr  
+			FROM tbl_college, tbl_office, tbl_administration WHERE tbl_administration.id = '$uid' AND 
+			tbl_administration.office = tbl_office.id AND tbl_office.college = tbl_college.id");
+			$c = $getCollege->row_array();
+			echo $cid = $c['id'];
+
+
 		$result = $this->db->query("SELECT tbl_curriculum.id as curricid, tbl_curriculum.description as curriculumdesc,`coursemajor`,`academicterm`, course, major, tbl_course.description as coursename, CONCAT(systart, '-', syend) as sy, yearlevel  
-			FROM `tbl_curriculum`, tbl_coursemajor, tbl_course, tbl_academicterm WHERE tbl_coursemajor.id = coursemajor AND tbl_course.id = course AND tbl_academicterm.id = academicterm AND major = 0 
-			AND tbl_academicterm.term = 1 GROUP BY coursemajor, tbl_curriculum.academicterm");
+			FROM `tbl_curriculum`, tbl_coursemajor, tbl_course, tbl_academicterm, tbl_college WHERE tbl_coursemajor.id = coursemajor AND tbl_course.id = course AND tbl_academicterm.id = academicterm AND major = 0 
+			AND tbl_academicterm.term = 1 AND tbl_course.college = '$cid' GROUP BY coursemajor, tbl_curriculum.academicterm");
 		return $result->result_array();
 	}
 	function getAllcurr2(){
+		$uid = $this->session->userdata('uid');
+		$getCollege = $this->db->query("SELECT tbl_college.id as id, tbl_college.description as descr  
+			FROM tbl_college, tbl_office, tbl_administration WHERE tbl_administration.id = '$uid' AND 
+			tbl_administration.office = tbl_office.id AND tbl_office.college = tbl_college.id");
+			$c = $getCollege->row_array();
+			echo $cid = $c['id'];
+
+
 		$result = $this->db->query("SELECT tbl_curriculum.id as curricid, tbl_curriculum.description as curriculumdesc,`coursemajor`,`academicterm`, course, major, CONCAT(tbl_course.description, ' (', tbl_major.description, ')') as coursename, CONCAT(systart, '-', syend) as sy, yearlevel  
 			FROM `tbl_curriculum`, tbl_coursemajor, tbl_course, tbl_academicterm, tbl_major WHERE tbl_coursemajor.id = coursemajor AND tbl_course.id = course AND tbl_academicterm.id = academicterm AND major != 0 AND tbl_major.id = major 
-			AND tbl_academicterm.term = 1 GROUP BY coursemajor, tbl_curriculum.academicterm");
+			AND tbl_academicterm.term = 1 AND tbl_course.college = '$cid' GROUP BY coursemajor, tbl_curriculum.academicterm");
 			return $result->result_array();
 	}
 	function getMatch($id,$cid)
