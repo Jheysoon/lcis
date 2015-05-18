4996c894780be63ae32efca33c316dc73ee2cc49
gregoriosonit <gregoriosonit@gmail.com>
2015-05-18 12:04:36 +0800

    Edited Curriculum.php

--- application/models/registrar/Curriculum.php
+++ application/models/registrar/Curriculum.php
@@ -75,11 +75,11 @@ class Curriculum extends CI_Model
 		$result = $this->db->query("SELECT tbl_curriculum.id as curricid, tbl_curriculum.description as curriculumdesc,`coursemajor`,`academicterm`, course, major, tbl_course.description as coursename, CONCAT(systart, '-', syend) as sy, yearlevel  
 			FROM `tbl_curriculum`, tbl_coursemajor, tbl_course, tbl_academicterm, tbl_college WHERE tbl_coursemajor.id = coursemajor AND tbl_course.id = course AND tbl_academicterm.id = academicterm AND major = 0 
 			AND tbl_academicterm.term = 1 AND (tbl_course.college = '$cid' OR tbl_course.college = '$cids' ) GROUP BY coursemajor, tbl_curriculum.academicterm");
-		return $result->result_array();
+			return $result->result_array();
 	}
 	function getAllcurr2(){
-		$uid = $this->session->userdata('uid');
-		$getCollege = $this->db->query("SELECT tbl_college.id as id, tbl_college.description as descr  
+			$uid = $this->session->userdata('uid');
+			$getCollege = $this->db->query("SELECT tbl_college.id as id, tbl_college.description as descr  
 			FROM tbl_college, tbl_office, tbl_administration WHERE tbl_administration.id = '$uid' AND 
 			tbl_administration.office = tbl_office.id AND tbl_office.college = tbl_college.id");
 			$c = $getCollege->row_array();
@@ -120,6 +120,4 @@ class Curriculum extends CI_Model
 	function insertsubj(){
 		echo 1;
 	}
-
-
 }
