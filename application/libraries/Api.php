6e643f01a029e9c826af110ab60caea9be7bc71b
Jayson Martinez <jlmartinez631@gmail.com>
2015-05-22 09:14:04 +0800

    fixed future error in getUserCollege

--- application/libraries/Api.php
+++ application/libraries/Api.php
@@ -21,18 +21,18 @@ class Api
             'dean/common_dean'
         ));
 
-        $col = $this->CI->common_dean->countAcam($this->session->userdata('uid'));
+        $col = $this->CI->common_dean->countAcam($this->CI->session->userdata('uid'));
         if($col > 0)
         {
-            $owner = $this->CI->common_dean->getColAcam($this->session->userdata('uid'));
+            $owner = $this->CI->common_dean->getColAcam($this->CI->session->userdata('uid'));
             return $college = $owner['college'];
         }
         else
         {
-            $c = $this->CI->common_dean->countAdmin($this->session->userdata('uid'));
+            $c = $this->CI->common_dean->countAdmin($this->CI->session->userdata('uid'));
             if($c > 0)
             {
-                $owner = $this->CI->common_dean->getColAdmin($this->session->userdata('uid'));
+                $owner = $this->CI->common_dean->getColAdmin($this->CI->session->userdata('uid'));
                 $o = $owner['office'];
                 $of = $this->CI->common_dean->getOffice($o);
                 return $college = $of['college'];
