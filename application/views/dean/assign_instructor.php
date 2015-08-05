e5bb6ce81060d2b0d502f02f87d8474ed4296037
Jayson Martinez <jlmartinez631@gmail.com>
2015-08-05 17:23:46 +0800

    assigning instructor

--- application/views/dean/assign_instructor.php
+++ application/views/dean/assign_instructor.php
@@ -8,10 +8,67 @@
             <?php
                 $owner = $this->api->getUserCollege();
                 $systemVal = $this->api->systemValue();
-                $cl = $this->db->get_where('tbl_classallocation',
-                        array('academicterm' => $systemVal['currentacademicterm'])
-                    )->result_array();
+                // $cl = $this->db->query("SELECT * FROM tbl_classallocation a,tbl_subject b
+                //     WHERE a.subject = b.id
+                //     AND b.owner = $owner
+                //     AND academicterm = {$systemVal['currentacademicterm']}")->result_array();
+
+                $instruc = $this->db->get_where('tbl_academic', array('college' => $owner))->result_array();
+
+                $cl = $this->db->query("SELECT b.code as code,b.descriptivetitle as title,a.id as cl_id,coursemajor,instructor FROM tbl_classallocation a, tbl_subject b WHERE a.subject = b.id AND academicterm = {$systemVal['currentacademicterm']} LIMIT 20")->result_array();
              ?>
+             <table class="table">
+                 <tr>
+                     <th>Subject</th>
+                     <th style="width:50%;">Course</th>
+                     <th>Instructor</th>
+                     <th>Action</th>
+                 </tr>
+                 <?php
+                    foreach($cl as $class)
+                    {
+                        ?>
+                        <tr>
+                            <td><?php echo $class['code'] ?></td>
+                            <td>
+                                <?php
+                                    if ($class['coursemajor'] != 0) {
+                                        $this->db->where('id', $class['coursemajor']);
+                                        $this->db->select('description');
+                                        $cc = $this->db->get('tbl_course')->row_array();
+                                        echo $cc['description'];
+                                    }
+                                    else
+                                        echo 'Not Available';
+                                ?>
+                            </td>
+                            <td>
+                                <select class="form-control" name="">
+                                    <?php if($class['instructor'] == 0) { ?>
+                                        <option value="option">No Instructor</option>
+                                    <?php
+                                    }
+                                    // auto check if the instructor is available for that day/period
+                                     foreach($instruc as $i){ ?>
+                                        <option value="<?php echo $i['id'] ?>">
+                                            <?php
+                                                $this->db->where('id', $i['id']);
+                                                $this->db->select('firstname,lastname,middlename');
+                                                $tt = $this->db->get('tbl_party')->row_array();
+                                                echo $tt['lastname'].', '.$tt['firstname'].' '.$tt['middlename'];
+                                            ?>
+                                        </option>
+                                    <?php } ?>
+                                </select>
+                            </td>
+                            <td>
+                                <button type="submit" class="btn btn-primary btn-sm" name="button">Add</button>
+                            </td>
+                        </tr>
+                <?php
+                    }
+                  ?>
+             </table>
 		</div>
 	</div>
 </div>
