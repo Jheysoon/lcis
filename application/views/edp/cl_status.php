536c68304338567805c4661a3da9f72c266671ef
Jayson Martinez <jlmartinez631@gmail.com>
2015-08-13 16:39:13 +0800

    fix some result if the classallocation status is 99

--- application/views/edp/cl_status.php
+++ application/views/edp/cl_status.php
@@ -1,6 +1,6 @@
-<div class="alert alert-info center-block" style="max-width:400px;">
+<div class="alert alert-info center-block" style="max-width:400px;text-align:center">
     <?php
         $systemVal = $this->api->systemValue();
      ?>
-     The classallocation is now in step no. <?php echo $systemVal['classallocationstatus'] + 1 ?>
+     The classallocation is now in step no. <?php echo ($systemVal['classallocationstatus'] == 99) ? '<br/>Class Allocation program is finished' : $systemVal['classallocationstatus'] + 1  ?>
 </div>
