f6a1e9209bc19f61c1029297a71fe22085247a1d
Jayson Martinez <jlmartinez631@gmail.com>
2015-08-18 11:00:03 +0800

    text sometimes does not render

--- application/views/edp/cl_status.php
+++ application/views/edp/cl_status.php
@@ -1,9 +1,12 @@
 <div class="alert alert-info center-block" style="max-width:400px;text-align:center">
     <?php
         $systemVal = $this->api->systemValue();
-        if($systemVal['classallocationstatus'] == 99)
+        if($systemVal['classallocationstatus'] == 99){
             echo '<br/>Class Allocation program is finished';
-        else
-            echo 'The classallocation is now in step no. '.$systemVal['classallocationstatus'] + 1;
+        }
+        else{
+            $r = $systemVal['classallocationstatus'] + 1;
+            echo 'The classallocation is now in step no. '.$r;
+        }
     ?>
 </div>
