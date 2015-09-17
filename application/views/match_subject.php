72cddfbc6596e5e926cd438ecc3bfc6619ab0867
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-17 11:36:45 +0800

    partial

--- application/views/match_subject.php
+++ application/views/match_subject.php
@@ -20,15 +20,106 @@
 
   </head>
 <body>
+    <nav class="navbar navbar-inverse nav-head" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <div>
+            <img class="img-logo" src="<?php echo base_url('assets/images/LC Logo.jpg'); ?>">
+              <h2 class="hd-title">
+                  <a class="title" href="<?php echo base_url(); ?>">LEYTE COLLEGES Information System</a>
+              </h2>
+          </div>
+      </div><!-- /.container-fluid -->
+    </nav>
+    <br><br><br>
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-                
+                <div class="panel p-body">
+    				<div class="panel-heading search">
+    					<div class="col-md-6">
+    					<h4>Curriculum Subjects</h4>
+    					</div>
+    				</div>
+                    <table class="table table-bordered">
+                        <thead>
+                            <th></th>
+                            <th>Code</th>
+                            <th>Descriptive Title</th>
+                            <th>Units</th>
+                        </thead>
+                        <tbody style="height:400px;overflow-y:auto;">
+                            <?php
+                                $r = $this->db->get('view_cur_subjects')->result_array();
+                                foreach($r as $sub)
+                                {
+                                    ?>
+                                    <tr>
+                                        <td>
+                                            <input type="radio" name="cur_sub[]" value="<?php echo $sub['id'] ?>">
+                                        </td>
+                                        <td>
+                                            <?php echo $sub['code'] ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $sub['descriptivetitle'] ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $sub['units'] ?>
+                                        </td>
+                                    </tr>
+                            <?php
+                                }
+                             ?>
+                        </tbody>
+                    </table>
+    			</div>
             </div>
             <div class="col-md-6">
-
+                <div class="panel p-body">
+    				<div class="panel-heading search">
+    					<div class="col-md-6">
+    					<h4>Legacy Subjects</h4>
+    					</div>
+    				</div>
+                    <table class="table table-bordered">
+                        <thead>
+                            <th></th>
+                            <th>Code</th>
+                            <th>Descriptive Title</th>
+                            <th>Units</th>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $r = $this->db->query("SELECT * FROM tbl_enrolment_legacy WHERE subject_id = 0 GROUP BY grouping ORDER BY grouping ASC LIMIT 200")->result_array();
+                                foreach($r as $rr)
+                                {
+                                    ?>
+                                    <tr>
+                                        <td>
+                                            <input type="checkbox" name="name[]" value="<?php echo $rr['id'] ?>">
+                                        </td>
+                                        <td>
+                                            <?php echo $rr['SUBNAME'] ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $rr['SUBTITLE'] ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $rr['UNITS'] ?>
+                                        </td>
+                                    </tr>
+                            <?php
+                                }
+                             ?>
+                        </tbody>
+                    </table>
+    			</div>
             </div>
         </div>
     </div>
+<script type="text/javascript">
+
+</script>
 </body>
 </html>
