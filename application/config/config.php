637de13a8e2a686266ecef29fde6cd6c04722aab
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-01 10:17:28 +0800

    align

--- application/config/config.php
+++ application/config/config.php
@@ -181,11 +181,11 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 | use segment based URLs.
 |
 */
-$config['allow_get_array'] = TRUE;
+$config['allow_get_array']      = TRUE;
 $config['enable_query_strings'] = FALSE;
-$config['controller_trigger'] = 'c';
-$config['function_trigger'] = 'm';
-$config['directory_trigger'] = 'd';
+$config['controller_trigger']   = 'c';
+$config['function_trigger']     = 'm';
+$config['directory_trigger']    = 'd';
 
 /*
 |--------------------------------------------------------------------------
@@ -353,13 +353,13 @@ $config['encryption_key'] = 'lcis-dev.com';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 0;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_driver']              = 'files';
+$config['sess_cookie_name']         = 'ci_session';
+$config['sess_expiration']          = 0;
+$config['sess_save_path']           = NULL;
+$config['sess_match_ip']            = FALSE;
+$config['sess_time_to_update']      = 300;
+$config['sess_regenerate_destroy']  = FALSE;
 
 /*
 |--------------------------------------------------------------------------
@@ -424,12 +424,12 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
-$config['csrf_token_name'] = 'csrf_token';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
-$config['csrf_expire'] = 7200;
-$config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_protection']      = FALSE;
+$config['csrf_token_name']      = 'csrf_token';
+$config['csrf_cookie_name']     = 'csrf_cookie_name';
+$config['csrf_expire']          = 7200;
+$config['csrf_regenerate']      = TRUE;
+$config['csrf_exclude_uris']    = array();
 
 /*
 |--------------------------------------------------------------------------
