0bb36a97fb820a136b672daf1bf27a27b09b0db8
Jayson Martinez <jlmartinez631@gmail.com>
2015-10-08 17:52:57 +0800

    nextacademicterm -> phaseterm

--- application/views/edp/studentStat.php
+++ application/views/edp/studentStat.php
@@ -17,7 +17,7 @@
 						<div class="alert alert-info center-block" id="confirmBox" style="max-width:400px;">
 							<strong> Do you want to run the student statistics for <br/>
 							<?php
-								$nnxt = $this->academicterm->findById($nxt['nextacademicterm']);
+								$nnxt = $this->academicterm->findById($nxt['phaseterm']);
 								echo $nnxt['systart'].' - '.$nnxt['syend'].' Term: '.$nnxt['term'];
 							?>
 						?</strong>
