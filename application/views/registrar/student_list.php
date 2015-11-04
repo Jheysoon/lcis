e0187d8d07baf509bfc652f2c25b4e6325d6c270
Jayson Martinez <jlmartinez631@gmail.com>
2015-11-04 09:15:22 +0800

    code cleanup and convert if to inline

--- application/views/registrar/student_list.php
+++ application/views/registrar/student_list.php
@@ -2,13 +2,13 @@
 	<div class="col-md-9 body-container">
 		<div class="panel p-body">
 		<div class="panel-heading search">
-			<div class="col-md-6">						
+			<div class="col-md-6">
 			<?php //if ($page == "updateOldStudents"): ?>
-				<h4>Student Information Management: List of Students</h4>						
+				<h4>Student Information Management: List of Students</h4>
 			<?php //else: ?>
-				<h4>Permanent Records: List of Students</h4>							
+				<h4>Permanent Records: List of Students</h4>
 			<?php //endif ?>
-			
+
 			</div>
 			<div class="col-md-6">
 
@@ -18,28 +18,28 @@
             <?php echo $this->session->flashdata('message'); ?>
             <div class="col-md-6">
             <?php
-                $config['base_url'] = base_url().'index.php/menu/registrar-student_list';
-                $config['total_rows'] = $this->enrollment->getRows();
-                $config['per_page'] = 15;
-                $config['num_links'] = 2;
-                $config['first_link'] = 'First';
-                $config['last_link'] = 'Last';
-                $config['first_tag_open'] = '<li>';
-                $config['first_tag_close'] = '</li>';
-                $config['last_tag_open'] = '<li>';
-                $config['last_tag_close'] = '</li>';
-                $config['num_tag_open'] = '<li>';
-                $config['num_tag_close'] = '</li>';
-                $config['cur_tag_open'] = '<li class="active"><a href="javascript:void(0);">';
-                $config['cur_tag_close'] = '</a></li>';
-                $config['next_tag_open'] = '<li>';
-                $config['next_tag_open'] = '</li>';
-                $config['prev_tag_open'] = '<li>';
-                $config['prev_tag_close'] = '</li>';
-                $config['next_tag_open'] = '<li>';
-                $config['next_tag_close'] = '</li>';
-                $config['prev_link'] = 'Prev';
-                $config['next_link'] = 'Next';
+                $config['base_url'] 		= base_url().'index.php/menu/registrar-student_list';
+                $config['total_rows'] 		= $this->enrollment->getRows();
+                $config['per_page'] 		= 15;
+                $config['num_links'] 		= 2;
+                $config['first_link'] 		= 'First';
+                $config['last_link'] 		= 'Last';
+                $config['first_tag_open'] 	= '<li>';
+                $config['first_tag_close'] 	= '</li>';
+                $config['last_tag_open'] 	= '<li>';
+                $config['last_tag_close'] 	= '</li>';
+                $config['num_tag_open'] 	= '<li>';
+                $config['num_tag_close'] 	= '</li>';
+                $config['cur_tag_open'] 	= '<li class="active"><a href="javascript:void(0);">';
+                $config['cur_tag_close'] 	= '</a></li>';
+                $config['next_tag_open'] 	= '<li>';
+                $config['next_tag_open'] 	= '</li>';
+                $config['prev_tag_open'] 	= '<li>';
+                $config['prev_tag_close'] 	= '</li>';
+                $config['next_tag_open'] 	= '<li>';
+                $config['next_tag_close'] 	= '</li>';
+                $config['prev_link'] 		= 'Prev';
+                $config['next_link'] 		= 'Next';
                 //$config['next_link'] = '<li><a href="#">&gt;</a></li>';
                 if(empty($param))
                 {
@@ -61,34 +61,16 @@
                 <label>Status Filter</label>
                 <?php $pos = $this->session->userdata('datamanagement'); ?>
                 <select class="form-control" name="status" id = "status">
-                    <?php 
-                        if($pos == 'E'){ ?>
-                        
-                    <option value="N" <?php if ($this->session->userdata('status') == 'N'): ?>
-                        selected
-                    <?php endif ?>>Not Open</option>
 
-                    
-                    <option value="O" <?php if ($this->session->userdata('status') == 'O'): ?>
-                        selected
-                    <?php endif ?>>Open</option>
-
-                    <option value="E" <?php if ($this->session->userdata('status') == 'E'): ?>
-                        selected
-                    <?php endif ?>>Editing</option>
+                    <?php if($pos == 'E') { ?>
+	                    <option value="N" <?php echo ($this->session->userdata('status') == 'N') ? 'selected' : ''  ?>>Not Open</option>
+	                    <option value="O" <?php echo ($this->session->userdata('status') == 'O') ? 'selected' : '' ?>>Open</option>
+                    	<option value="E" <?php echo ($this->session->userdata('status') == 'E') ? 'selected' : '' ?>>Editing</option>
                     <?php } ?>
 
-                    <option value="S" <?php if ($this->session->userdata('status') == 'S'): ?>
-                        selected
-                    <?php endif ?>>Submitted</option>
-                    
-
-                    <option value="C" <?php if ($this->session->userdata('status') == 'C'): ?>
-                        selected
-                    <?php endif ?>>Confirmed</option>
-                    <option value="R" <?php if ($this->session->userdata('status') == 'R'): ?>
-                        selected
-                    <?php endif ?>>Returned</option>
+                    <option value="S" <?php echo ($this->session->userdata('status') == 'S') ? 'selected' : '' ?>>Submitted</option>
+                    <option value="C" <?php echo ($this->session->userdata('status') == 'C') ? 'selected' : '' ?>>Confirmed</option>
+                    <option value="R" <?php echo ($this->session->userdata('status') == 'R') ? 'selected' : '' ?>>Returned</option>
                 </select>
 
                 <div class="form-group">
@@ -104,17 +86,12 @@
 		<div class="panel-body">
             <input type="hidden" name="param" value="<?php echo $param; ?>"/>
     		<div id="studlist_wrapper" class="table-responsive">
-                <?php 
-                    $this->load->view('registrar/ajax/tbl_studlist',$data);
-                 ?>
+                <?php $this->load->view('registrar/ajax/tbl_studlist',$data); ?>
     		</div>
-                    
+
             <ul class="pagination">
-                <?php
-                    echo $this->pagination->create_links();
-                ?>
+                <?php echo $this->pagination->create_links(); ?>
             </ul>
 		</div>
 		</div>
 	</div>
-</div>
