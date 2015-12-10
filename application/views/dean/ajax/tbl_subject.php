b77729720724979c422588ccfc4a7238a1392234
Jayson Martinez <jlmartinez631@gmail.com>
2015-12-10 14:46:40 +0800

    :art:

--- application/views/dean/ajax/tbl_subject.php
+++ application/views/dean/ajax/tbl_subject.php
@@ -8,8 +8,8 @@
 	</tr>
 	<?php
 		$subj = $this->subject->subjectWhere($college);
-		foreach($subj as $sub)
-		{
+		
+		foreach ($subj as $sub) {
 	?>
 	<tr>
 		<td><?php echo $sub['code']; ?></td>
@@ -18,27 +18,27 @@
 		<td>
 		<?php
 			$col = $this->common_dean->getCollege($sub['id']);
-			if($col == 0)
-			{
+			
+			if ($col == 0) {
 				echo 'Not Been Assigned';
-			}
-			else
-			{
+			} else {
 				$t = $this->common_dean->getColName($col);
 				echo $t;
 			}
 		 ?>
-		 </td>
+		</td>
 		<td>
-		<?php if($col == 0 OR $col == $college){ ?>
-			<a class="btn btn-success btn-xs btn-block" href="/edit_subject/<?php echo $sub['id']; ?>">Edit &nbsp;<span class="glyphicon glyphicon-pencil"></span></a>
-			<a class="btn btn-danger btn-xs btn-block delete_subject" data-subjectname="<?php echo $sub['code'].' '.$sub['descriptivetitle']; ?>" data-param="<?php echo $sub['id']; ?>" href="/delete_subject/<?php echo $sub['id']; ?>">Delete <span class="glyphicon glyphicon-trash"></span></a>
-		<?php }else{ ?>
-		<a href="/edit_subject/<?php echo $sub['id']; ?>/view" class="btn btn-info btn-xs btn-block">View</a>
+		<?php if ($col == 0 OR $col == $college) { ?>
+			<a class="btn btn-success btn-xs btn-block" href="/edit_subject/<?php echo $sub['id']; ?>">
+				Edit &nbsp;<span class="glyphicon glyphicon-pencil"></span>
+			</a>
+			<a class="btn btn-danger btn-xs btn-block delete_subject" data-subjectname="<?php echo $sub['code'].' '.$sub['descriptivetitle']; ?>" data-param="<?php echo $sub['id']; ?>" href="/delete_subject/<?php echo $sub['id']; ?>">
+				Delete <span class="glyphicon glyphicon-trash"></span>
+			</a>
+		<?php } else { ?>
+			<a href="/edit_subject/<?php echo $sub['id']; ?>/view" class="btn btn-info btn-xs btn-block">View</a>
 		<?php } ?>
 		</td>
 	</tr>
-	<?php
-		}
-	 ?>
+	<?php } ?>
 </table>
