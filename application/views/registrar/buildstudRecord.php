ea71642d4451431b3bdd86009babb0294e6d6e3f
Jayson Martinez <jlmartinez631@gmail.com>
2015-12-14 10:20:37 +0800

    extract add subject

--- application/views/registrar/buildstudRecord.php
+++ application/views/registrar/buildstudRecord.php
@@ -496,60 +496,20 @@
                                     <?php if ($position != 'C' or $position != 'B'): ?>
 	                                    <tr>
 	                                        <td colspan="6">
-	                                            <div class="modal fade" id="myModal<?php echo '_ac-'.$academicterm.'_sch-'.$school; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	                                                <div class="modal-dialog modal-sm">
-	                                                    <div class="modal-content">
-	                                                        <form class="frm-add-subj">
-	                                                            <div class="modal-header">
-	                                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-	                                                                <h3 class="modal-title" id="myModalLabel">Add Subject</h3>
-	                                                            </div>
-	                                                            <div class="modal-body">
-	                                                                <input type="hidden" name="partyid" value="<?php echo $partyid; ?>"/>
-	                                                                <input type="hidden" name="academictermid" value="<?php echo $academicterm; ?>"/>
-	                                                                <input type="hidden" name="enrolmentid" value="<?php echo $enrolmentid; ?>"/>
-	                                                                <input type="hidden" name="schoolid" value="<?php echo $school; ?>"/>
-	                                                                <label>Subject</label><br/>
-                                                                    <select class="form-control" name="add_subj">
-                                                                        <?php
-                                                                            $this->db->where('own', $school);
-                                                                            $s = $this->db->get('tbl_subject')->result();
-                                                                            foreach ($s as $subject) {
-                                                                         ?>
-                                                                        <option value="<?php echo $subject->id ?>"><?php echo $subject->code.' | '.$subject->descriptivetitle ?></option>
-                                                                        <?php } ?>
-                                                                    </select>
-	                                                                <br/>Grade<br/>
-	                                                                <select name="sub_grade" class="form-control">
-	                                                                    <?php
-	                                                                        $g = $this->grade->getAllGrade();
-	                                                                        foreach($g as $gg)
-	                                                                        {
-	                                                                            ?>
-	                                                                            <option value="<?php echo $gg['id']; ?>">
-                                                                                    <?php
-                                                                                        if ($gg['value']  == 0.00) {
-                                                                                            echo $gg['description'];
-                                                                                        } else {
-                                                                                            echo $gg['value'];
-                                                                                        }
-                                                                                    ?>
-                                                                                </option>
-	                                                                        <?php
-	                                                                        }
-	                                                                    ?>
-	                                                                </select>
-	                                                            </div>
-	                                                            <div class="modal-footer">
-	                                                                <button type="submit" class="btn btn-primary">Save</button>
-	                                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-	                                                            </div>
-	                                                        </form>
-	                                                    </div>
-	                                                </div>
-	                                            </div>
-                                                <?php if ($this->session->userdata('status') != 'S' AND ($position != 'C' or $position != 'B')): ?>
-                                                        <a href="<?php echo '_ac-'.$academicterm.'_sch-'.$school; ?>" class="btn btn-primary pull-right modal-add-subj-grade"><span class="glyphicon glyphicon-plus"></span> Add Subject</a>
+                                                <?php
+                                                    $data['academicterm']   = $academicterm;
+                                                    $data['school']         = $school;
+                                                    $data['partyid']        = $partyid;
+                                                    $data['enrolmentid']    = $enrolmentid;
+                                                    
+                                                    $this->load->view('registrar/form_permanent_record/add_subject', $data);
+                                                    
+                                                    if ($this->session->userdata('status') != 'S' AND ($position != 'C' or $position != 'B')): 
+                                                ?>
+                                                        <a href="<?php echo '_ac-'.$academicterm.'_sch-'.$school; ?>" class="btn btn-primary pull-right modal-add-subj-grade">
+                                                            <span class="glyphicon glyphicon-plus"></span> 
+                                                            Add Subject
+                                                        </a>
                                                 <?php endif ?>
 	                                           </td>
 	                                    </tr>
