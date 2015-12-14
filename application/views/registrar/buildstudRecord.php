30e05d8768f2c91cf8fcfc46a9e26f9b52ef8007
Jayson Martinez <jlmartinez631@gmail.com>
2015-12-14 10:48:33 +0800

    extract form

--- application/views/registrar/buildstudRecord.php
+++ application/views/registrar/buildstudRecord.php
@@ -514,45 +514,26 @@
                                                 <?php endif ?>
 	                                           </td>
 	                                    </tr>
- 							<?php endif ?>
+							<?php endif ?>
                                 <?php endforeach ?>
                             </table>
                         <?php endforeach ?>
                         <br />
-                        <?php }
+                        <?php 
+                    }
                             if (!is_array($result)) {
                                 $partyid = $id;
                             }
-
-	                        $getflag = $this->common->theflag($partyid);
-                            $status = $this->log_student->getLatestTm($partyid);
-                            if ($getflag < 1 AND ($position == 'C' or $position == 'B')):
-
-                            ?>
-                                <form action="/registrar/insert_flag" method="POST">
-                                    <input type="hidden" name="url" value="<?php echo current_url(); ?>"/>
-                                    <input type="hidden" name="tm" value="<?php echo $status; ?>"/>
-                                    <input type="hidden" name="flag_status" value="C"/>
-                                    <input type="hidden" name="partyid" value="<?php echo $partyid; ?>">
-                                    <input type="submit" class="btn btn-primary pull-right" value="Confirm" onclick="return confirm('Are you sure?')">
-                                </form>
-                                
-                                <form action="/registrar/insert_flag" method="POST">
-                                    <input type="hidden" name="flag_status" value="R"/>
-                                    <input type="hidden" name="partyid" value="<?php echo $partyid; ?>">
-                                    <input type="submit" class="btn btn-primary" value="Return to Clerk">
-                                </form>
-                                
-                        <?php elseif ($this->session->userdata('status') != 'S'): ?>
-                                  <form action="/registrar/insert_flag" method="POST">
-                                    <input type="hidden" name="url" value="<?php echo current_url(); ?>"/>
-                                    <input type="hidden" name="flag_status" value="S"/>
-                                    <input type="hidden" name="partyid" value="<?php echo $partyid; ?>">
-                                    <input type="submit" class="btn btn-primary pull-right" value="Submit" onclick="return confirm('Are you sure?')">
-                                </form>
-                        <?php endif ?>
-
-              	  </div>
+	                        
+                            $status             = $this->log_student->getLatestTm($partyid);
+                            $param['status']    = $status;
+                            $param['partyid']   = $partyid;
+                            $param['position']  = $position;
+                            
+                            // insert flag form
+                            $this->load->view('registrar/form_permanent_record/insert_flag', $param);
+                        ?>
+            	  </div>
                 <!-- /div class table-responsive -->
 
             <!--</div>-->
