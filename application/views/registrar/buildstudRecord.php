74aa5fa407b0dabe3196b48e913c654f67497048
Jayson Martinez <jlmartinez631@gmail.com>
2015-12-24 16:37:52 +0800

    organize adding academicterm

--- application/views/registrar/buildstudRecord.php
+++ application/views/registrar/buildstudRecord.php
@@ -295,74 +295,17 @@
                 <!-- div class table-responsive -->
 
                 <!-- modal add academicterm -->
-                <?php if (($position != 'C' OR $position != 'B') OR $this->session->userdata('status') != 'S'): ?>
+                <?php
+                    if (($position != 'C' OR $position != 'B') OR $this->session->userdata('status') != 'S'):
 
-                <div class="modal fade" id="modal_academicterm">
-                    <div class="modal-dialog modal-sm">
-                        <div class="modal-content">
-                            <form id="frm_add_academicterm">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <h3 class="modal-title">Add Academic Term</h3>
-                                </div>
-                                <div class="modal-body">
-                                    <input type="hidden" name="partyid" value="<?php echo $partyid; ?>"/>
-                                    School
-                                    <select name="school_id" class="form-control">
-                                        <?php
-                                            $sch = $this->party->college_school();
+                        // add academicterm modal
+                        $this->load->view('registrar/form_permanent_record/add_academicterm', array('partyid' => $partyid));
+                    endif;
 
-                                            foreach ($sch as $s) {
-                                        ?>
-                                                <option value="<?php echo $s['id']; ?>"><?php echo $s['firstname']; ?></option>
-                                        <?php } ?>
-                                    </select>
-                                    Course
-                                    <select name="course_id" class="form-control">
-                                        <?php
-                                            $courses = $this->course->getAllCourse();
-
-                                            foreach ($courses as $c) {
-                                        ?>
-                                            <option value="<?php echo $c['id']; ?>">
-                                                <?php
-                                                    $cour = $this->course->getCourse($c['id']);
-                                                    $major = $this->course->getMajor($c['major']);
-                                                    echo $cour.' '.$major;
-                                                ?>
-                                            </option>
-                                        <?php } ?>
-                                    </select>
-                                    School Year
-                                    <select name="sy_id" class="form-control">
-                                        <?php
-                                            $sy = $this->academicterm->all();
-
-                                            foreach ($sy as $sy1) {
-
-                                                if($sy1['term'] == '1')
-                                                    $sem = 'FIRST SEMESTER';
-                                                elseif($sy1['term'] == '2')
-                                                    $sem = 'SECOND SEMESTER';
-                                                else
-                                                    $sem = 'SUMMER CLASS';
-                                        ?>
-                                            <option value="<?php echo $sy1['id']; ?>"><?php echo $sy1['systart'].'-'.$sy1['syend'].' '.$sem; ?></option>
-                                        <?php } ?>
-                                    </select>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="submit" class="btn btn-primary">Save</button>
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                </div>
-                            </form>
-                        </div><!-- /.modal-content -->
-                    </div><!-- /.modal-dialog -->
-                </div><!-- /.modal -->
-                <?php endif ?>
-                <?php if($this->session->userdata('datamanagement') =='E' AND $this->session->userdata('status') != 'S'){ ?>
-                      <input type="button" id="add_academicterm" class="btn btn-primary pull-right" value="Add Academicterm"/>
-                <span class="clearfix"></span>
+                    if ($this->session->userdata('datamanagement') =='E' AND $this->session->userdata('status') != 'S') {
+                ?>
+                    <input type="button" id="add_academicterm" class="btn btn-primary pull-right" value="Add Academicterm"/>
+                    <span class="clearfix"></span>
                 <?php } ?>
                 <br/>
 
