a6ff81f698249b064e5598af5cdbaaa772927727
Jayson Martinez <jlmartinez631@gmail.com>
2015-12-27 18:00:46 +0800

    correct html

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,7 +3,7 @@
           <div class="col-md-12">
             <label class="text-center">
             <a class="foot-link" href="http://www.leytecolleges.edu.ph" target="blank">www.leytecolleges.edu.ph</a> |
-            &copy <?php echo date('Y'); ?> |
+            &copy; <?php echo date('Y'); ?> |
             All Rights Reserved <?php echo date('Y'); ?>
             </label>
           </div>
